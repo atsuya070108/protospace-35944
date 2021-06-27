61696bfd3632368d5894dea12509f149fa4e5d0d
atsuya070108 <85215859+atsuya070108@users.noreply.github.com>
2021-06-27 11:41:35 +0900

    first commit

---
+++ app/models/prototype.rb
@@ -0,0 +1,11 @@
+class Prototype < ApplicationRecord
+  validates :image, presence: true
+  validates :title, presence: true
+  validates :catch_copy, presence: true
+  validates :concept, presence: true
+
+  has_one_attached :image
+  belongs_to :user
+  has_many :comments, dependent: :destroy
+  
+end
