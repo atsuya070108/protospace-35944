9e294ce75e0f4db05f5d22e95c7550924fbe63ab
atsuya070108 <85215859+atsuya070108@users.noreply.github.com>
2021-06-27 13:08:29 +0900

    修正

--- db/schema.rb
+++ db/schema.rb
@@ -56,7 +56,7 @@ ActiveRecord::Schema.define(version: 2021_06_25_123426) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "name", null: false
+    t.string "name", default: "", null: false
     t.text "profile", null: false
     t.text "occupation", null: false
     t.text "position", null: false
